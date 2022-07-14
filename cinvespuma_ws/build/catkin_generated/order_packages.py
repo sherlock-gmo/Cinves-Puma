5e1379301313b0a9754628c41dadae580365d997
sherlock-gmo <ogonzalez31416@gmail.com>
2022-07-14 14:28:11 -0500

    unecessary files erased

--- cinvespuma_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/sherlock/Cinves-Puma/cinvespuma_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/sherlock/EK_AutoNOMOS_Sim/devel;/home/sherlock/TMR-2022-AutoModelCar/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/sherlock/EK_AutoNOMOS_Sim/devel;/home/sherlock/TMR-2022-AutoModelCar/catkin_ws/devel;/opt/ros/noetic' != '' else []
